a1118beb91a40c39cf02917bbc77083cdb3cb9d4
Felix Hietaranta <49163212+FelixHiet@users.noreply.github.com>
2022-09-20 20:53:56 +0300

    2nd commit

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+const options = {
+  params: {
+    bl_latitude: "11.847676",
+    tr_latitude: "12.838442",
+    bl_longitude: "109.095887",
+    tr_longitude: "109.149359",
+  },
+  headers: {
+    "X-RapidAPI-Key": "c9b789da23msh2245eede78b6263p1f314ejsn4934b2b33c41",
+    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+  },
+};
+
+export const getPlacesData = async () => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, options);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,9 +1,21 @@
+import React from "react";
+
+const PlaceDetails = ({ place }) => {
+  return <h1>{place.name}</h1>;
+};
+
+export default PlaceDetails;
+
+/*
+
 import React from 'react'
 
-const PlaceDetails = () => {
+const PlaceDetails = ({ place }) => {
   return (
-    <div>PlaceDetails</div>
+    <h1>{place.name}</h1> // takes the name from the List.jsx const places
   )
 }
 
 export default PlaceDetails
+
+*/

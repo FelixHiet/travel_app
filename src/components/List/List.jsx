a6c00cf8c961b78991b5bc725744ebe4e70db5d1
Felix Hietaranta <49163212+FelixHiet@users.noreply.github.com>
2022-09-21 11:00:07 +0300

    App now fetches the nearby restaurants

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -13,16 +13,10 @@ import PlaceDetails from "../PlaceDetails/PlaceDetails";
 
 import useStyles from "./styles";
 
-const List = () => {
+const List = ({ places }) => {
   const classes = useStyles();
   const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState("");
-
-  const places = [
-    { name: "Cool place" },
-    { name: "Quality beer" },
-    { name: "Gordon Ramsays best steaks" },
-  ];
 
   return (
     <div className={classes.container}>
